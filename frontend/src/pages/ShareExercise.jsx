7aa851ce0bb383e9cfb37d7b1d0a2d03baba1bde
sunray4 <118241363+sunray4@users.noreply.github.com>
2024-08-31 19:06:18 -0700

    combine share exercise functions

--- frontend/src/pages/ShareExercise.jsx
+++ frontend/src/pages/ShareExercise.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import generateUUID from '../utils/uuidKeyGenerator.jsx';
 import { generatePDF } from '../utils/generatePDF.jsx';
+import { UploadExercise } from '../utils/uploadExercise.jsx';
 
-import { useNavigate } from 'react-router-dom';
-
-export default function ShareExercise() {
+export default function EditAndShareExercise() {
   const [subject, setSubject] = useState('');
   const [unit, setUnit] = useState('');
   const [year, setYear] = useState('');
@@ -13,34 +13,144 @@ export default function ShareExercise() {
   const [description, setDescription] = useState('');
   const [images, setImages] = useState([]);
   const [imgEdited, setImgEdited] = useState([]);
+  const [index, setIndex] = useState(0);
   const navigate = useNavigate();
+  const canvasRefs = useRef([]);
+  const canvasInvsRefs = useRef([]);
+  const imgRef = useRef([]);
 
-  function handleImageChange(e) {
+  // Handle image file selection
+  const handleImageChange = (e) => {
     const selectedImages = Array.from(e.target.files);
     const imgUrls = selectedImages.map(file => URL.createObjectURL(file));
     setImages(imgUrls);
-  }
+  };
 
-  // useEffect(() => {
-  //   const imageSources = location.state?.imgEdited || [];
-  //   setImgEdited(imageSources);
-  // }, [location.state]);
+  // Initialize canvas and image elements
+  useEffect(() => {
+    images.forEach((imgSrc, index) => {
+      const canvas = canvasRefs.current[index];
+      const ctx = canvas.getContext("2d");
+      
+      const image = new Image();
+      image.src = imgSrc;
+      
+      image.onload = () => {
+        const proportion = image.width / image.height;
+        canvas.width = 500;
+        canvas.height = 500 / proportion;
+        const scaleWidth = canvas.width / image.width;
+        const scaleHeight = canvas.height / image.height;
+        const scale = Math.min(scaleWidth, scaleHeight);
+
+        // Calculate the position to center the image in the canvas
+        const x = (canvas.width / 2) - (image.width / 2) * scale;
+        const y = (canvas.height / 2) - (image.height / 2) * scale;
+
+        // Clear the canvas and draw the scaled image
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.drawImage(image, x, y, image.width * scale, image.height * scale);
+
+        imgRef.current[index] = image;
+
+
+        const canvasInvs = canvasInvsRefs.current[index];
+        const ctx2 = canvasInvs.getContext("2d");
+        canvasInvs.width = canvas.width;
+        canvasInvs.height = canvas.height;
+        ctx2.fillStyle = "white";
+        ctx2.fillRect(0, 0, canvasInvs.width, canvasInvs.height);
+        canvasInvs.style.visibility = "hidden";
+      };
+
+      
+    });
+  }, [images]);
+
+  const onmousedown = (e, index) => {
+    const canvas = canvasRefs.current[index];
+    const startX = e.nativeEvent.offsetX;
+    const startY = e.nativeEvent.offsetY;
+    const element = document.createElement('div');
+    element.className = 'rectangle';
+    element.style.position = 'absolute';
+    element.style.background = "green";
+    element.style.left = `${startX}px`;
+    element.style.top = `${startY}px`;
+    canvas.parentNode.appendChild(element);
+
+    canvasRefs.current[index].element = { element, startX, startY };
+  };
+
+  const onmouseup = (e, index) => {
+    const canvas = canvasRefs.current[index];
+    const { element, startX, startY } = canvasRefs.current[index].element || {};
+    if (!element) return;
+
+    const ctx = canvas.getContext("2d");
+    const canvasInvs = canvasInvsRefs.current[index];
+    const ctx2 = canvasInvs.getContext("2d");
+
+    const curX = e.nativeEvent.offsetX;
+    const curY = e.nativeEvent.offsetY;
+
+    ctx.beginPath();
+    ctx.rect(startX, startY, curX - startX, curY - startY);
+    ctx.stroke();
+
+    ctx2.drawImage(imgRef.current[index], startX, startY, curX - startX, curY - startY, startX, startY, curX - startX, curY - startY);
+
+  };
+
+  const onmousemove = (e, index) => {
+    const canvas = canvasRefs.current[index];
+    const { element, startX, startY } = canvasRefs.current[index].element || {};
+    if (!element) return;
+
+    const curX = e.nativeEvent.offsetX;
+    const curY = e.nativeEvent.offsetY;
+    element.style.width = `${curX - startX}px`;
+    element.style.height = `${curY - startY}px`;
+  };
+
+  const applyChanges = (index) => {
+    const canvas = canvasRefs.current[index];
+    const canvasInvs = canvasInvsRefs.current[index];
+    const dataUrl = canvasInvs.toDataURL('image/jpeg', 0.9);
+
+    setImgEdited(prevImgUrls => ([...prevImgUrls, dataUrl]));
+
+    // Remove green boxes
+    const parent = canvas.parentNode;
+    const boxes = parent.querySelectorAll('.rectangle');
+    boxes.forEach(box => parent.removeChild(box));
+
+    canvas.style.visibility = "hidden";
+    canvasInvs.style.visibility = "visible";
+
+    // const ctx = canvas.getContext('2d');
+    // ctx.clearRect(0, 0, canvas.width, canvas.height);
+    setIndex(index + 1);
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     const key = generateUUID();
     const ansPDF = await generatePDF(images);
+    const exPDF = await generatePDF(imgEdited);
 
-    // Navigate to EditImagePage with the images
-    navigate('/edit-image', { state: { images: images, key:key, subject:subject, unit:unit, year:year, teacher:teacher, school:school, description:description, } });
-    window.open('/edit-image');
-
+    try {
+      await UploadExercise(key, subject, unit, year, teacher, school, description, exPDF, ansPDF);
+      navigate('/'); // Redirect after successful upload
+    } catch (error) {
+      console.log("Error running upload exercise", error);
+    }
   };
 
   return (
     <div className='flex justify-center items-center min-h-screen p-4'>
       <div className='text-white rounded-md w-full max-w-md p-6 flex flex-col'>
-        <h1 className='text-xl font-bold text-center mb-5'>Upload a past paper</h1>
+        <h1 className='text-xl font-bold text-center mb-5'>Upload and Edit Exercise</h1>
         <form onSubmit={handleSubmit} className='flex flex-col'>
           <div className='text-base mb-5'>
             <input
@@ -118,6 +228,32 @@ export default function ShareExercise() {
           </div>
           <button type="submit" className='w-full bg-[#7481FF] text-white py-2 rounded-md hover:bg-[#8F97E0]'>Submit</button>
         </form>
+        <div className='flex flex-wrap justify-center items-center gap-4 mt-5'>
+        {images.length > 0 && (
+          <div>
+            {images.map((imgSrc, index) => (
+              <div key={index} className='relative'>
+                <canvas
+                  ref={el => canvasRefs.current[index] = el}
+                  width="500"
+                  height="500"
+                  style={{ border: '1px solid #000' }}
+                  onMouseDown={(e) => onmousedown(e, index)}
+                  onMouseUp={(e) => onmouseup(e, index)}
+                  onMouseMove={(e) => onmousemove(e, index)}
+                />
+                <canvas
+                  ref={el => canvasInvsRefs.current[index] = el}
+                  width="500"
+                  height="500"
+                  style={{ border: '1px solid #000', position: 'absolute', top: 0, left: 0 }}
+                />
+                <button onClick={() => applyChanges(index)}>Apply Changes</button>
+              </div>
+            ))}
+          </div>
+        )}
+        </div>
       </div>
     </div>
   );
