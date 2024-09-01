24184f409256144bb42087b3a218d992a7b0da98
sunray4 <118241363+sunray4@users.noreply.github.com>
2024-09-01 00:34:43 -0700

    fix git pull errors

--- frontend/src/pages/ShareExercise.jsx
+++ frontend/src/pages/ShareExercise.jsx
@@ -19,6 +19,7 @@ export default function EditAndShareExercise() {
   const canvasRefs = useRef([]);
   const canvasInvsRefs = useRef([]);
   const imgRef = useRef([]);
+  const submitButtonRef = useRef(null);
 
   // Handle image file selection
   const handleImageChange = (e) => {
@@ -50,12 +51,14 @@ export default function EditAndShareExercise() {
 
         // Clear the canvas and draw the scaled image
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.drawImage(image, x, y, image.width * scale, image.height * scale);
+        // ctx.drawImage(image, x, y, image.width * scale, image.height * scale);
+        ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
 
         imgRef.current[index] = image;
 
 
         const canvasInvs = canvasInvsRefs.current[index];
+        canvasRefs.current[index].start = true;
         const ctx2 = canvasInvs.getContext("2d");
         canvasInvs.width = canvas.width;
         canvasInvs.height = canvas.height;
@@ -70,68 +73,147 @@ export default function EditAndShareExercise() {
 
   const onmousedown = (e, index) => {
     const canvas = canvasRefs.current[index];
-    const startX = e.nativeEvent.offsetX;
-    const startY = e.nativeEvent.offsetY;
-    const element = document.createElement('div');
-    element.className = 'rectangle';
-    element.style.position = 'absolute';
-    element.style.background = "green";
-    element.style.left = `${startX}px`;
-    element.style.top = `${startY}px`;
-    canvas.parentNode.appendChild(element);
-
-    canvasRefs.current[index].element = { element, startX, startY };
-  };
-
-  const onmouseup = (e, index) => {
-    const canvas = canvasRefs.current[index];
-    const { element, startX, startY } = canvasRefs.current[index].element || {};
-    if (!element) return;
-
-    const ctx = canvas.getContext("2d");
     const canvasInvs = canvasInvsRefs.current[index];
+    const ctx = canvas.getContext("2d");
     const ctx2 = canvasInvs.getContext("2d");
 
     const curX = e.nativeEvent.offsetX;
     const curY = e.nativeEvent.offsetY;
+    const element = document.createElement('div');
+    element.className = 'rectangle';
+    element.style.position = 'absolute';
+    element.style.background = "rgba(0, 128, 0, 0.5)";
+    element.style.left = `${curX}px`;
+    element.style.top = `${curY}px`;
+    canvas.parentNode.appendChild(element);
+    
+    canvasRefs.current[index].start = !canvasRefs.current[index].start;
 
-    ctx.beginPath();
-    ctx.rect(startX, startY, curX - startX, curY - startY);
-    ctx.stroke();
+    if (canvasRefs.current[index].start) {
+      const { element:element, curX:startX, curY:startY } = canvasRefs.current[index].element;
+      console.log('Start Coordinates:', startX, startY);
+      console.log('Current Coordinates:', curX, curY);
 
-    ctx2.drawImage(imgRef.current[index], startX, startY, curX - startX, curY - startY, startX, startY, curX - startX, curY - startY);
+      // const width = Math.abs(curX - startX);
+      // const height = Math.abs(curY - startY);
 
+      // // Check if the dimensions are valid
+      // if (width <= 0 || height <= 0) {
+      //     console.error('Invalid width or height for getImageData.');
+      //     return;
+      // }
+
+      ctx.beginPath();
+      ctx.rect(startX, startY, curX - startX, curY - startY);
+      ctx.stroke();
+
+      try {
+          // Retrieve image data and put it on the invisible canvas
+          const imageData = ctx.getImageData(startX, startY, curX - startX, curY - startY);
+          ctx2.putImageData(imageData, startX, startY);
+      } catch (error) {
+          console.error('Error retrieving image data:', error);
+      }
+
+      }
+      else {
+        canvasRefs.current[index].element = { element, curX, curY };
+      }
+    };
+
+    const onmouseup = async (e, index) => {
+      // const canvas = canvasRefs.current[index];
+      // const canvasInvs = canvasInvsRefs.current[index];
+      
+      // // Declare startX and startY outside the block
+      // let startX, startY;
+  
+      //     // Assign startX and startY from the element
+      //     ({ startX, startY } = canvasRefs.current[index].element);
+  
+  
+      // if (!canvas || !canvasInvs) return;
+  
+      // const ctx = canvas.getContext("2d");
+      // const ctx2 = canvasInvs.getContext("2d");
+  
+      // // Ensure the canvas has dimensions
+      // if (canvas.width === 0 || canvas.height === 0) {
+      //     console.error('Canvas dimensions are not set.');
+      //     return;
+      // }
+  
+      // const curX = e.nativeEvent.offsetX;
+      // const curY = e.nativeEvent.offsetY;
+      
+      // console.log('Start Coordinates:', startX, startY);
+      // console.log('Current Coordinates:', curX, curY);
+  
+      // setPassCoordinates((prevC) => ([...prevC, {x:curX, y:curY}]));
+  
+      // if (passCoordinates.length % 2 == 0 && passCoordinates.length > 1) {
+      //   const width = Math.abs(curX - passCoordinates[passCoordinates.length - 2].x);
+      //   const height = Math.abs(curY - passCoordinates[passCoordinates.length - 2].y);
+  
+      //   // Check if the dimensions are valid
+      //   if (width <= 0 || height <= 0) {
+      //       console.error('Invalid width or height for getImageData.');
+      //       return;
+      //   }
+  
+      //   ctx.beginPath();
+      //   ctx.rect(passCoordinates[passCoordinates.length - 2].x, passCoordinates[passCoordinates.length - 2].y, width, height);
+      //   ctx.stroke();
+  
+      //   try {
+      //       // Retrieve image data and put it on the invisible canvas
+      //       const imageData = ctx.getImageData(passCoordinates[passCoordinates.length - 2].x, passCoordinates[passCoordinates.length - 2].y, width, height);
+      //       ctx2.putImageData(imageData, passCoordinates[passCoordinates.length - 2].x, passCoordinates[passCoordinates.length - 2].y);
+      //   } catch (error) {
+      //       console.error('Error retrieving image data:', error);
+      //   }
+      // }
+  
+      
   };
+  
+  
+    const onmousemove = (e, index) => {
+      const canvas = canvasRefs.current[index];
+      const { element, startX, startY } = canvasRefs.current[index].element || {};
+      if (!element) return;
+  
+      const curX = e.nativeEvent.offsetX;
+      const curY = e.nativeEvent.offsetY;
+      element.style.width = `${curX - startX}px`;
+      element.style.height = `${curY - startY}px`;
+    };
+  
+    const applyChanges = (index) => {
+      const canvas = canvasRefs.current[index];
+      const canvasInvs = canvasInvsRefs.current[index];
+      const dataUrl = canvasInvs.toDataURL('image/jpeg', 0.9);
+  
+      setImgEdited(prevImgUrls => ([...prevImgUrls, dataUrl]));
+  
+      // Remove green boxes
+      // const parent = canvas.parentNode;
+      // const boxes = parent.querySelectorAll('.rectangle');
+      // boxes.forEach(box => parent.removeChild(box));
+  
+      canvas.style.visibility = "hidden";
+      canvasInvs.style.visibility = "visible";
+  
+      // const ctx = canvas.getContext('2d');
+      // ctx.clearRect(0, 0, canvas.width, canvas.height);
+      setIndex(index + 1);
+    };
+     
 
-  const onmousemove = (e, index) => {
-    const canvas = canvasRefs.current[index];
-    const { element, startX, startY } = canvasRefs.current[index].element || {};
-    if (!element) return;
-
-    const curX = e.nativeEvent.offsetX;
-    const curY = e.nativeEvent.offsetY;
-    element.style.width = `${curX - startX}px`;
-    element.style.height = `${curY - startY}px`;
-  };
-
-  const applyChanges = (index) => {
-    const canvas = canvasRefs.current[index];
-    const canvasInvs = canvasInvsRefs.current[index];
-    const dataUrl = canvasInvs.toDataURL('image/jpeg', 0.9);
-
-    setImgEdited(prevImgUrls => ([...prevImgUrls, dataUrl]));
-
-    // Remove green boxes
-    const parent = canvas.parentNode;
-    const boxes = parent.querySelectorAll('.rectangle');
-    boxes.forEach(box => parent.removeChild(box));
-
-    canvas.style.visibility = "hidden";
-    canvasInvs.style.visibility = "visible";
-
-    // const ctx = canvas.getContext('2d');
-    // ctx.clearRect(0, 0, canvas.width, canvas.height);
-    setIndex(index + 1);
+  const handleSubmitButtonClick = () => {
+    if (submitButtonRef.current) {
+      submitButtonRef.current.click();
+    }
   };
 
   const handleSubmit = async (e) => {
@@ -147,6 +229,7 @@ export default function EditAndShareExercise() {
       console.log("Error running upload exercise", error);
     }
   };
+
 
   return (
     <div className='flex justify-center items-center min-h-screen p-4'>
@@ -184,7 +267,7 @@ export default function EditAndShareExercise() {
               Choose File
             </label>
           </div>
-          <button type="submit" className='w-full bg-[#7481FF] text-white py-2 rounded-md hover:bg-[#8F97E0]'>Submit</button>
+          <button ref={submitButtonRef} type="submit" className='hidden'>Submit</button>
         </form>
         <div className='flex flex-wrap justify-center items-center gap-4 mt-5'>
         {images.length > 0 && (
@@ -213,6 +296,7 @@ export default function EditAndShareExercise() {
         )}
         </div>
       </div>
+      <button onClick={handleSubmitButtonClick} type="submit" className='w-full bg-[#7481FF] text-white py-2 rounded-md hover:bg-[#8F97E0]'>Submit</button>
     </div>
   );
 }
